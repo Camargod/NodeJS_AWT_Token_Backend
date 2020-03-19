71cdb21e0cc03647dbb6d9694f693496d7ede939
Gabriel Silva de Camargo <38335652+Camargod@users.noreply.github.com>
2020-03-19 10:22:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const routes = require("./src/routes");
 const cors = require("cors");
-mongoose.connect("mongodb+srv://creeplays:Biel210801@omnistack10-bkvir.mongodb.net/FindMyDev?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://creeplays:<pwd>@omnistack10-bkvir.mongodb.net/FindMyDev?retryWrites=true&w=majority",{
     useUnifiedTopology:true,
     useNewUrlParser:true
 }).then(console.log("---Mongoose Conectado---"));
