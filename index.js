9c3a1b58c2cb99a7bac1d615003c9b08d6b6775a
Gabriel <creeplays@gmail.com>
2020-03-19 18:17:31 -0300

    senhas separadas.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const routes = require("./src/routes");
 const cors = require("cors");
-mongoose.connect("mongodb+srv://creeplays:Biel210801@omnistack10-bkvir.mongodb.net/FindMyDev?retryWrites=true&w=majority",{
+const authConfig = require('./src/config/auth')
+
+mongoose.connect(`mongodb+srv://creeplays:${authConfig.mongodbPwd}@omnistack10-bkvir.mongodb.net/AutenticationTest?retryWrites=true&w=majority`,{
     useUnifiedTopology:true,
     useNewUrlParser:true
-}).then(console.log("---Mongoose Conectado---"));
+}).then(console.log("--- Mongoose Conectado ---"));
 
 app.use(cors());
 app.use(express.json());
